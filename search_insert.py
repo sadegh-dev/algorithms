4adaec9012ceda17e78d5e329bc7bbc7c361e73f
charlie <sadegh.rez@gmail.com>
2021-12-20 17:52:49 +0330

    search_insert.py

---
+++ search_insert.py
@@ -0,0 +1,24 @@
+def search_insert(arr, num):
+    le = len(arr)
+    result = None
+
+    for i in range(0,le) :
+        if num < arr[i]:
+            result = i
+            break
+        elif num == arr[i]:
+            result = i 
+            break
+    
+    if result == None :
+        result = le
+
+    return result
+
+
+
+print( search_insert([1,3,5,6], 0) )
+print( search_insert([1,3,5,6], 1) )
+print( search_insert([1,3,5,6], 2) )
+print( search_insert([1,3,5,6], 5) )
+print( search_insert([1,3,5,6], 7) )
