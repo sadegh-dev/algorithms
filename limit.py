58fc592323c3c5048dac5ae6bc0a6baf58471018
sadegh-dev <sadegh.rez@gmail.com>
2022-05-27 20:06:26 +0430

    edit limit.py

--- limit.py
+++ limit.py
@@ -16,7 +16,7 @@ def limit (arr, min=None, max=None):
         result = arr.copy()
     return result
 
-
+print(limit([1,3,4,5,6,7,8,9,10]))
 print(limit([1,3,4,5,6,7,8,9,10], 3, 8))
 print(limit([1,3,4,5,6,7,8,9,10], 7))
 print(limit([1,3,4,5,6,7,8,9,10], max=6))
