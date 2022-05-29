a966e81856a144de3f4ceb3c5b4eb7a79077ba04
sadegh-dev <sadegh.rez@gmail.com>
2022-05-29 19:16:03 +0430

    #binary_serach -> definition left_edge, right_edge, middle

--- binary_search.py
+++ binary_search.py
@@ -3,10 +3,17 @@ Log2(N)
 
 """
 
+def binary_search(the_list, value):
+    left_edge , right_edge = 0 , len(the_list) -1
+    while left_edge <= right_edge:
+        middle = left_edge + (right_edge) -1
+        if the_list[middle] == value :
+            return middle
+        
+
+
+
+
 
 a_list = [10,20,30,40,50,60,70,80,90]
-
-def binary_search(the_list, value):
-    pass
-
-
+value = 40
