ab5be8e29c505034d92079ec401b64e615ab6983
sadegh-dev <sadegh.rez@gmail.com>
2022-06-02 10:33:34 +0430

    #linear_search -complete

--- linear_search.py
+++ linear_search.py
@@ -1,4 +1,17 @@
 
 
 def linear_search(arr, item):
-    pass
+    for i in arr :
+        if i == item :
+            return arr.index(item)
+    return -1
+    
+
+
+
+ar = [20,30,60,25,98,46]
+item = 90
+
+result = linear_search(ar, item)
+
+print(result)
