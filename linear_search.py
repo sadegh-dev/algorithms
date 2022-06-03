693d2216514359c6ecab00342130ffe9f7bd372e
sadegh-dev <sadegh.rez@gmail.com>
2022-06-03 12:32:03 +0430

    #linear_search -develop solution2

--- linear_search.py
+++ linear_search.py
@@ -1,13 +1,22 @@
 
-
+# Solution1
 def linear_search(arr, item):
     for i in arr :
         if i == item :
             return arr.index(item)
-    return -1
+    return False
     
 
+# Solution2
 
+def linear_search2(arr, item):
+    try: 
+        return arr.index(item)
+    except :
+        return False
+
+
+##############################
 
 ar = [20,30,60,25,98,46]
 item = 90
--- test.py
+++ test.py
@@ -1,27 +1,22 @@
-names = ['charlie', 'katty', 'joe', 'matew', 'josef']
-ages = [20, 35, 75, 63, 27, 43]
-data = [True, 'katty', 25, '33', 'charlie', False, 35]
+
+
+def linear_search(arr, item):
+    try: 
+        return arr.index(item)
+    except :
+        return False
 
 
 
 
-arr = ['a','b','c','d','b','a','g','a','d','b']
-
-def top_one3(arr):
-    key_result = list()
-    result = list()
-    rres = list()
-
-    for x in arr :
-        if x not in key_result :
-            key_result.append(x)
-            result.append(arr.count(x))
 
 
-    max_ = max(result)
 
-    for k, r in zip(key_result, result):
-        if r == max_ :
-            rres.append(k)
 
-    return list((rres, max_))
+
+arr = [1,3,5,6,9,2,5,4,6,9]
+item = 50
+
+result = linear_search(arr, item)
+
+print(result)
