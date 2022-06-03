fdb9031cb46cddc5e8ec5504c6cf47a593ff4485
sadegh-dev <sadegh.rez@gmail.com>
2022-06-03 14:57:03 +0430

    #bead_sort -complete

---
+++ bead_sort.py
@@ -0,0 +1,22 @@
+
+
+
+def bead_sort(arr):
+    result = list()
+    result.append(arr[0])
+    for x in arr[1:] :
+        for a in result :
+            if x <= a :
+                result.insert(result.index(a), x)
+                break
+        else :
+            result.append(x)
+    return result 
+        
+
+
+arr = [6,80,5,20,3,10,9,20]
+
+result = bead_sort(arr)
+
+print(result)
