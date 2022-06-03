ee4a6fd6cb5933b7e325a362ef0d70f4b22de1e0
sadegh-dev <sadegh.rez@gmail.com>
2022-06-03 14:20:48 +0430

    #a1z26 -decode function

--- a1z26.py
+++ a1z26.py
@@ -7,8 +7,17 @@ def encode_ (string_):
     return result
 
 
+def decode_q (coding):
+    result = ""
+    for x in coding :
+        result += chr(x)
+    return result
+
 
 
 txt = 'hello world !'
 result = encode_(txt)
 print(result)
+
+result_txt = decode_q(result)
+print(result_txt)
