60d24dec59fdc8341e97b8f0df16a32bfd42b1f0
sadegh-dev <sadegh.rez@gmail.com>
2022-06-03 13:11:11 +0430

    #limit -develop solution2

--- limit.py
+++ limit.py
@@ -1,3 +1,5 @@
+# Solution1 
+
 def limit (arr, min=None, max=None):
     result = []
     if max and min :
@@ -15,6 +17,25 @@ def limit (arr, min=None, max=None):
     else :
         result = arr.copy()
     return result
+
+
+# Solution2 
+# (MIN and MAX must be members of the list)
+
+def limit(arr, min_=None, max_=None):
+    try:
+        mi = arr.index(min_)
+    except:
+        mi = 0
+    try:
+        mx = arr.index(max_)+1
+    except:
+        mx = len(arr)
+    return arr[mi:mx]
+
+
+#####################################
+
 
 print(limit([1,3,4,5,6,7,8,9,10]))
 print(limit([1,3,4,5,6,7,8,9,10], 3, 8))
--- test.py
+++ test.py
@@ -1,22 +1,29 @@
 
+# solution2 
+# (MIN and MAX must be members of the list)
+def limit(arr, min_=None, max_=None):
+    try:
+        mi = arr.index(min_)
+    except:
+        mi = 0
+    try:
+        mx = arr.index(max_)+1
+    except:
+        mx = len(arr)
+    return arr[mi:mx]
 
-def linear_search(arr, item):
-    try: 
-        return arr.index(item)
-    except :
-        return False
 
 
 
+arr = [10,20,30,40,50,60,70,80,90,100,110]
+min_ = 30
+max_ = 90
 
+result = limit(arr, min_, max_)
+print(result)
 
+result = limit(arr, min_ )
+print(result)
 
-
-
-
-arr = [1,3,5,6,9,2,5,4,6,9]
-item = 50
-
-result = linear_search(arr, item)
-
+result = limit(arr, max_ = max_)
 print(result)
