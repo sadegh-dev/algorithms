c6750d7de8ea8c92aeaafdcce9efe45a757094aa
sadegh-dev <sadegh.rez@gmail.com>
2022-06-03 14:06:08 +0430

    #is_isomorphic -complete develop

--- is_isomorphic.py
+++ is_isomorphic.py
@@ -2,13 +2,19 @@ def is_isomorphic(x,y):
     if not (len(x) == len(y)):
         return False
 
+    ref = dict()
+    for xx , yy in zip(x,y):
+        pos = ref.setdefault(xx,yy)
+        if pos != yy :
+            return False
 
+    return True
 
 
 ###########################
 
 x = "helloe"
-y = "yippti"
+y = "yippqi"
 
 result = is_isomorphic(x,y)
 
--- test.py
+++ test.py
@@ -1,27 +1,4 @@
 
 
-def is_isomorphic(x,y):
-    if not (len(x) == len(y)):
-        return False
-    
-    ref = dict()
-    for xx , yy in zip(x,y):
-        pos = ref.setdefault(xx,yy)
-        if pos != yy :
-            return False
-
-    return True
-
-
-##############################3
-
-x = "helloe"
-y = "yipptp"
-
-result = is_isomorphic(x,y)
-print(result)
-
-
-
 
 
