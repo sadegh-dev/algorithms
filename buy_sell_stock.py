7a00516a84bda70209bebf180833185dc49ccda8
sadegh-dev <sadegh.rez@gmail.com>
2022-06-04 11:57:25 +0430

    #buy_sell_stock for

--- buy_sell_stock.py
+++ buy_sell_stock.py
@@ -5,5 +5,16 @@
 
 """
 
+def buy_sell_stock(arr):
+    statics_ = dict()
+    for p, x in enumerate(arr[1:]) :
+        diff_ = x -arr[p-1]
+        print(diff_) 
 
+
+arr = [2, 6, 7, 4, 3, 2, 8]
+
+buy_sell_stock(arr)
+
+#print(result)
 
