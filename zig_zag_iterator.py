ced6642e1a1a9e1be7ae1f7fcc3ecf4e816d4a10
sadegh-dev <sadegh.rez@gmail.com>
2022-06-04 14:05:38 +0430

    #zig_zag_iterator -complete develop solution2

--- zig_zag_iterator.py
+++ zig_zag_iterator.py
@@ -2,14 +2,14 @@
     zig zag iterator
     [a(0), b(0), a(1), b(1), a(2), b(2), ... ]
 
-    Ex:
+    Ex1:
     [10, 30, 90, 110, 70]
     [20, 60, 40, 100, 60]
     => [10, 20, 30, 60, 90, 40, 110, 100, 70, 60]
 
 """
 
-# Solution1 --------------------------
+# Solution1 [ with zip function ] #
 
 def zig_zag_iterator_1(arr1, arr2) :
     result = list()
@@ -26,12 +26,34 @@ def zig_zag_iterator_1(arr1, arr2) :
     return result
 
 
+# Solution2 [ without zip function ] #
+
+from copy import deepcopy
+
+def zig_zag_iterator_2(arr1, arr2) :
+    result = list()
+    diff_ = len(arr1) - len(arr2)
+    if diff_ < 0 :
+        c = deepcopy(arr1)
+        arr1 = deepcopy(arr2)
+        arr2 = deepcopy(c)
+    
+    #always here -> len(arr1) > len(arr2)
+    for x in range(len(arr2)) :
+        result.append(arr1[x])
+        result.append(arr2[x])
+    
+    if abs(diff_):
+        result.extend(arr1[-abs(diff_):])
+
+    return result
+
 ####################################
 
 l1 = [10, 30, 90, 66]
-l2 = [20, 60, 40, 50, 600]
+l2 = [20, 60, 40, 50,800,900]
 
-result = zig_zag_iterator_1(l1, l2)
+result = zig_zag_iterator_2(l1, l2)
 
 print(result)
 
