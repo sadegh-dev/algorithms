cd0da17f507c59bc2a5890b130f44a921d4161ce
sadegh-dev <sadegh.rez@gmail.com>
2022-06-07 13:00:03 +0430

    #one_timepad_cipher -develop continue...

--- one_timepad_cipher.py
+++ one_timepad_cipher.py
@@ -20,12 +20,51 @@ def create_3bits():
     return result
 
 
-def create_leters26_3bits ():
+def alphabet_3bits():
+    import string
+    alphabet_list = list(string.ascii_lowercase)
     result = list()
-    for x in range(26):
-        result.append(create_3bits())
+    for alp in alphabet_list:
+        the_bits = create_3bits()
+        result.append(list([alp, the_bits]))
     return result
 
 
-letters_26 = create_leters26_3bits()
+###### one_timepad_cipher ###########
+
+def do_(char_, key_):
+    the_bits = list_alphabet_3bits[char_][1]
+    for cc,kk in zip(the_bits, key_):
+        if cc == '0' and kk == '0' :
+            return '0'
+        elif cc == '1' and kk == '0' :
+            return '1'
+        elif cc == '0' and kk == '1' :
+            return '1'
+        elif cc == '1' and kk == '1' :
+            return '0'
+
+
+def cipher_(txt_):
+    keys_ = list()
+    result =''
+    for tt in txt_ :
+        the_key = create_3bits()
+        result += do_(tt,the_key)
+        keys_.append(the_key)
+
+
+    return (list(keys_, result))
+
+
+def decrypt_(keys_, cipher_txt):
+    pass
+
+
+##################################
+
+
+list_alphabet_3bits = alphabet_3bits() 
+
+
 
