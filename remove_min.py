1ce3a487b03224739c6bc310d4f80a4c6fe28d4f
sadegh-dev <sadegh.rez@gmail.com>
2022-06-07 11:20:10 +0430

    #remove min -complete solution1

--- remove_min.py
+++ remove_min.py
@@ -5,6 +5,18 @@
 
 """
 
+
+# Solution 1
+
+def remove_min2(arr):
+    result = arr[0]
+    for x in arr[1:] :
+        if x < result :
+            result = x
+    arr.remove(result)
+    return result
+
+
 # Solution 2
 
 def remove_min(arr):
@@ -17,6 +29,6 @@ def remove_min(arr):
 
 arr = [9,6,2,1,3,5,7]
 
-print( remove_min(arr) )
+print( remove_min2(arr) )
 print('##############')
 print(arr)
