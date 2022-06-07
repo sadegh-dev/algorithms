9ab10407facd15a28373c69056b0a0023f4ad98c
sadegh-dev <sadegh.rez@gmail.com>
2022-06-07 11:13:00 +0430

    #remove_min -definition base function

---
+++ remove_min.py
@@ -0,0 +1,19 @@
+"""
+    [9,6,2,1,3,5,7] -> 1
+
+    remove from array and return minimum data.
+
+"""
+
+def remove_min(arr):
+    pass
+
+
+
+##############################3
+
+arr = [9,6,2,1,3,5,7]
+
+print( remove_min(arr) )
+print('##############')
+print(arr)
