6196b1f22b3a9a0f3a067699526caed4cddae21b
sadegh-dev <sadegh.rez@gmail.com>
2022-06-10 19:54:08 +0430

    #two_sum -develop base function

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+"""
+    two sum
+    [2, 7, 11, 15] , 22 -> [1,3]
+
+"""
+
+def two_sum(list_, num):
+    pass
+
+
+
+
+list_a = [2,5,11,15]
+sum_num = 22
+
+result = two_sum(list_a, sum_num)
+
+print(result)
