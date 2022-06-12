ff2b29a7b377e54e5179d939a5ee494a39cedda5
sadegh-dev <sadegh.rez@gmail.com>
2022-06-12 23:30:24 +0430

    #rotate -create base def

---
+++ rotate.py
@@ -0,0 +1,19 @@
+"""
+    rotate
+        rotate("yellow",2) return "llowye"
+        rotate("yellow",5) return "yellow"
+        rotate("yellow",6) return "ellowy"
+        rotate("yellow",7) return "llowye" 
+"""
+
+
+def rotate(txt, num):
+    pass
+
+
+result = rotate("hello",3)
+print(result)
+
+result = rotate("hello",7)
+print(result)
+
