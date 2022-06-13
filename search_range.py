5042107077bf53fa7a20beaa5bd106c64b1b6814
sadegh-dev <sadegh.rez@gmail.com>
2022-06-13 14:01:09 +0430

    #serach_range -definition

---
+++ search_range.py
@@ -0,0 +1,19 @@
+"""
+    search range
+        [5,7,7,8,8,8,10], 8  => [3,5]
+        [5,7,7,8,8,8,10], 11 => [None,None]
+
+"""
+
+def search_range(list_,num):
+    pass
+
+
+################################
+
+
+result = search_range([5,7,7,8,8,8,10],8)
+print(result)
+
+result = search_range([5,7,7,8,8,8,10],11)
+print(result)
