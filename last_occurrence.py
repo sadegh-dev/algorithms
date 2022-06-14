4051cfc95b83f2c9dd1c964e19e52f705a07e9f2
sadegh-dev <sadegh.rez@gmail.com>
2022-06-14 14:46:15 +0430

    #last_occurence -develop complete

--- last_occurrence.py
+++ last_occurrence.py
@@ -5,16 +5,18 @@
 """
 
 def last_occurrence(list_, num) :
-    ll = len(the_list)
-    for x in range(ll):
-        pass
+    ll = len(list_)
+    for x in range(ll-1,-1,-1):
+        if list_[x] == num :
+            return x
+    return None
 
 
 ############################
 
 
 the_list = [2,3,4,5,5,6,6,6,7,8,8,8,9]
-num = 7
+num = 6
 
 result = last_occurrence(the_list, num)
 print(result)
