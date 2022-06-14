d4072195c2d304fa6f0afea69a933b369d657e9f
sadegh-dev <sadegh.rez@gmail.com>
2022-06-14 14:40:03 +0430

    #last_occurrence -definition blank def

---
+++ last_occurrence.py
@@ -0,0 +1,19 @@
+"""
+    last occurrence
+        [2,3,4,5,5,6,6,6,7,8,8,8,9] , 6 => 7
+
+"""
+
+def last_occurrence(list_, num) :
+    pass
+
+
+############################
+
+
+the_list = [2,3,4,5,5,6,6,6,7,8,8,8,9]
+num = 7
+
+result = last_occurrence(the_list, num)
+print(result)
+
